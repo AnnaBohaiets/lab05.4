1ca373b396892687a6648059a5e5cebef66e74fb
AnnaBohaiets <grynko.n@gmail.com>
2023-10-29 13:06:27 +0200

    first commit

---
+++ UnitTest05.4/UnitTest05.4.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_05.4/lab05.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest054
+{
+	TEST_CLASS(UnitTest054)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double g;
+			g = S2(1, 6);
+			Assert::AreEqual(g, 9.79631, 0.0001);
+		}
+	};
+}
